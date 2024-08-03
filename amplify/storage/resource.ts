76b8527557319cd02d8b1e733b7ba28f8368993e
raj <panchalraj36@gmail.com>
2024-08-03 23:26:25 +0530

    fixes

--- amplify/storage/resource.ts
+++ amplify/storage/resource.ts
@@ -2,4 +2,10 @@ import { defineStorage } from '@aws-amplify/backend';
 
 export const storage = defineStorage({
   name: 'edtech-storage',
+  access: (allow) => ({
+    'media/*': [
+      allow.groups(['STUDENT']).to(['read']),
+      allow.groups(['TEACHER']).to(['read', 'write', 'delete']),
+    ],
+  }),
 });
